6bebdd080dc3fe55d150acb762dca41a9b9ae801
Degun <eorjs9568@naver.com>
2016-10-17 16:43:35 +0900

    Bootstrap Javascript ~ Tooltip

---
+++ sample-javascript/js/tooltip.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  $('a').tooltip('show');
+
+  var i = 0;
+ setTimeout(function() {
+   $('a').tooltip('hide');
+  //  console.log("setTimeout : ", i);
+   setInterval(function() {
+     if (i < 6) {
+       $('a').tooltip('toggle');
+     }
+    //  console.log("setInterval : ", i);
+     i++;
+   }, 500);
+ }, 3000);
+
+
+
+});
+
+ // setTimeout(function() {
+ //   $('a').tooltip('show');
+ // }, 3500);
+ //
+ //
+ //  setTimeout(function() {
+ //    $('a').tooltip('hide');
+ //  }, 4000);
+ //
+ //
+ // setTimeout(function() {
+ //   $('a').tooltip('show');
+ // }, 4500);
+ //
+ //
+ //  setTimeout(function() {
+ //    $('a').tooltip('hide');
+ //  }, 5000);
+ //  });
