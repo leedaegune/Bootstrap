4ce5d6eb6987672c0c43aa027c94df1422a7a070
Degun <eorjs9568@naver.com>
2016-10-18 16:20:27 +0900

    Bootstrap Javascript ~ Collapse Sample

--- sample-javascript/js/tapnav.js
+++ sample-javascript/js/tapnav.js
@@ -1,7 +1,23 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 //
 // });
 
 $(function(){
   $('#myTab a:last').tab('show');
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (event) {
+    $(event.relatedTarget).find('i').remove();
+    $(event.target).prepend('<i class="fa fa-arrow-right"></i>');
+    });
+    // console.log(event.target); // newly activated tab
+    // console.log(event.relatedTarget); // previous active tab
+    //   var href = $(event.target).attr('href');
+    //   console.log(href);
+    //   console.log($(href).html());
+    // });
+    $('#myTab').on('shown.bs.tab', function(event){
+      $(event.relatedTarget).find('i').css('display', 'none');
+      $(event.target).find('i').css('display', 'inline');
+    });
+});
 });
